2206abbf519e4edf24a4ba1738e1615e7986df63
AbreuStrahd <esdsabreu@gmail.com>
2023-04-25 13:12:34 -0300

    atualizando styles e sobre nós

---
+++ scripts/script_sobre.js
@@ -0,0 +1,11 @@
+function modoEscuro() {
+    document.getElementById("body").className = "modo_escuro"
+  }
+
+  function modoBordo() {
+    document.getElementById("body").className = "modo_bordo"
+  }
+
+  function modoNoite() {
+    document.getElementById("body").className = "modo_noite"
+  }
